0e8473aa5eeea91416f3a13f33b4508070e75436
parksaehyun <psh666777@naver.com>
2024-06-01 15:09:54 +0900

    Attendance 서브메뉴 구성 중

---
+++ src/main/java/org/choongang/attendance/constants/SubMenu.java
@@ -0,0 +1,9 @@
+package org.choongang.attendance.constants;
+
+import org.choongang.global.constants.Menu;
+
+public enum SubMenu implements Menu {
+    ATTENDANCE_CHECK, // 출석 조회
+    ATTENDANCE_REGISTER, // 출석 등록
+    ATTENDANCE_DELETE // 출석 삭제
+}
---
+++ src/test/java/attendanceTest/SelectData.java
@@ -0,0 +1,4 @@
+package attendanceTest;
+
+public class SelectData {
+}
