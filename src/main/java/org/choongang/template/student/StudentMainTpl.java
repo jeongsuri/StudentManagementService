c04aa5a4e5892e86eafaac90f1041bf95a2220f6
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-06-03 20:15:23 +0900

    초기 코드로 다시 변경

--- src/main/java/org/choongang/template/student/StudentMainTpl.java
+++ src/main/java/org/choongang/template/student/StudentMainTpl.java
@@ -6,10 +6,10 @@ public class StudentMainTpl implements Template {
     @Override
     public String getTpl() {
         StringBuffer sb = new StringBuffer(1000);
-        sb.append("학생관리\n");
-        sb.append("1. 학생 등록\n");
-        sb.append("2. 학생 조회\n");
-        sb.append("3. 메인 메뉴\n");
+        sb.append("             <학생관리>\n");
+        sb.append("           1. 학생 등록\n");
+        sb.append("           2. 학생 조회\n");
+        sb.append("           3. 메인 메뉴\n");
         return sb.toString();
     }
 }
---
+++ src/main/java/org/choongang/template/student/StudentTpl.java
@@ -0,0 +1,16 @@
+package org.choongang.template.student;
+
+import org.choongang.template.Template;
+import org.choongang.template.Templates;
+
+public class StudentTpl implements Template {
+    @Override
+    public String getTpl() {
+        StringBuffer sb = new StringBuffer(1000);
+        sb.append("1. 학생 등록\n");
+        sb.append("2. 학생 조회\n");
+        sb.append(Templates.getInstance().line());
+
+        return sb.toString();
+    }
+}
