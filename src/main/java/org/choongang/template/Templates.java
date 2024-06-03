35069c9e84dcd0eb896641074dbb2c106661a268
parksaehyun <psh666777@naver.com>
2024-06-03 11:23:36 +0900

    서브메뉴 MVC 중 뷰 수정중

--- src/main/java/org/choongang/template/Templates.java
+++ src/main/java/org/choongang/template/Templates.java
@@ -1,67 +1,86 @@
 package org.choongang.template;
 
 import org.choongang.attendance.constants.AttendanceMenu;
+import org.choongang.global.Menu;
 import org.choongang.global.constants.MainMenu;
 import org.choongang.template.attendance.AttendanceTpl;
+import org.choongang.template.attendance.DeleteAttendanceTpl;
+import org.choongang.template.attendance.InsertAttendanceTpl;
+import org.choongang.template.attendance.SelectAttendanceTpl;
 import org.choongang.template.main.MainTpl;
 import org.choongang.template.reservation.ReservationTpl;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Supplier;
 
 public class Templates {
     private static Templates instance;
     private Map<MainMenu, Template> tpls;
-    private Map<AttendanceMenu, Template> attendanceTpls;
+
     private Templates() {
         tpls = new HashMap<>();
-        attendanceTpls = new HashMap<>();
     }
+
     public static Templates getInstance(){
         if(instance == null){
             instance = new Templates();
         }
         return instance;
     }
-    public void render(MainMenu mainMenu) {
-        render(mainMenu, null);
+
+    public void render(Menu menu) {
+        render(menu, null);
     }
 
-    public <T> void render(MainMenu mainMenu, T data) {
-        System.out.println(find(mainMenu,data).getTpl());
-    }
-
-    public <T> void render(AttendanceMenu attendanceMenu, T data) {
-        System.out.println(find(attendanceMenu, data).getTpl());
+    public void render(Menu menu, Supplier<String> hook) {
+        System.out.println(find(menu, hook).getTpl());
     }
 
 
-    public <T> Template find(MainMenu mainMenu, T data){
-        Template tpl = tpls.get(mainMenu);
-        if(tpl != null){
+    public Template find(Menu menu, Supplier<String> hook) {
+        Template tpl = tpls.get(menu);
+        if (tpl != null) {
             return tpl;
         }
         if (menu instanceof AttendanceMenu) {
             AttendanceMenu attendanceMenu = (AttendanceMenu) menu;
-         }
-        switch (mainMenu){
-            case RESERVATION: tpl = new ReservationTpl(); break;
-            case ATTENDANCE: tpl = new AttendanceTpl(); break;
-            default: tpl = new MainTpl();
+            switch(attendanceMenu) {
+                case SELECT: tpl = new SelectAttendanceTpl();
+                case INSERT: tpl = new InsertAttendanceTpl();
+                case DELETE: tpl = new DeleteAttendanceTpl(); break;
+            }
+        } else {
+            MainMenu mainMenu = (MainMenu)menu;
+            switch (mainMenu) {
+                case MAIN:
+                    tpl = new   MainTpl();
+                    break;
+                case RESERVATION:
+                    tpl = new ReservationTpl();
+                    break;
+                case ATTENDANCE:
+                    tpl = new AttendanceTpl();
+                    break;
+                default:
+                    tpl = new MainTpl();
+            }
         }
 
-        if (data != null) {
-            tpl.setData(data);
+        if (hook != null) {
+            tpl.addHook(hook);
         }
 
-        tpls.put(mainMenu,tpl);
+        tpls.put(menu, tpl);
+
         return tpl;
     }
 
-    public String line(){
-        return "------------------------------------------\n";
+    public String line() {
+        return "-----------------------------------\n";
     }
 
-    public String doubleLine(){
-        return "==========================================\n";
+    public String doubleLine() {
+        return "===================================\n";
     }
 }
